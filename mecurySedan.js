8bf7c86ff0e56521fe7d0be385ec9b521d570bcb
Veny Cordova <venycordova@gmail.com>
2022-10-03 09:54:57 -0400

    added the properties

--- mecurySedan.js
+++ mecurySedan.js
@@ -4,8 +4,14 @@ const VehicleModule = require("./vehicle").Vehicle
 class car extends VehicleModule {
     constructor(make, model, year, color, mileage){
         super(make, model, year, color, mileage);
-        
+        this.maximumPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        scheduleService = false;
     }
+    
 }
 
 
