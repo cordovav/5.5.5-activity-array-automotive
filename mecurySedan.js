2d6c51b9b73ee2880f07aefc6ee101a744ebf5fa
Veny Cordova <venycordova@gmail.com>
2022-10-13 19:16:20 -0400

    fixed issues

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,13 +1,16 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("vehicle.js")
+//import { Vehicle as VehicleModule } from '.vehicle.js';
+const VehicleModule = require("./vehicle.js")
+
+//include .Vehicle to have it extend
 
 
 //Create a Car class that extends the Vehicle class (located in the vehicle.js).
-class car extends VehicleModule {
+class Car extends VehicleModule.Vehicle {
     //Add constructor
     constructor(make, model, year, color, mileage){
         //added super keyword to call parent class constructor
-        super(make, model, year, color, mileage);
+        super(make, model, year, color, mileage); 
         //added the properties 
         this.maximumPassengers = 5;
         this.passenger = 0;
@@ -17,33 +20,48 @@ class car extends VehicleModule {
         this.scheduleService = false;
     }
 //Add the methods with psuedo logic
-
-//added load passanger method
-loadPassenger(num){
-    if(this.passengers < this.maximumPassenger);
-    console.log("There is room for more passengers");
-    return availableRoom == true;
-}
-
-//added the start method
-start() {
-    if (this.fuel > 0) {
-        console.log("engine started...!!!");
-        return this.started = true;
-    } else {
-        console.log("engine cannot start...");
-        return this.started = false;
-    }
-}
 //added schedule service method
-scheduleService(mileage){
-    if(this.mileage > 3000 );
-    console.log("time for maintenance");
-    return scheduleService = true;
-}
+
+    checkService(){
+        if(this.mileage > 3000 ){
+            this.scheduleService = true
+            console.log("time for maintenance");
+            return this.scheduleService;
+        }    
+    }
+    //added the start method
+    start() {
+        if (this.fuel > 0) {
+            console.log("engine started...!!!");
+            return this.started = true;
+            } else {
+                console.log("engine cannot start...NO FUEL!");
+                return this.started = false;
+        }
+    }
+//added load passanger method
+    loadPassenger(num){
+        if(this.passengers < this.maximumPassenger){
+            if((num + this.passenger) <= this.maxPassengers){
+                this.passenger = num;
+                return this.passenger;
+                } else {
+                    console.log(this.modle + " " + this.make + "not enough space for all passengers")
+                }
+            } else {
+                console.log(this.model + " " + this.make + "is full");
+            }    
+
+        }
+
+
 }
 
 
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+let myCar = new Car('mercury', 'rad_sedan', '2002', 'white', 5000)
+myCar.start()
+myCar.loadPassenger(5)
+myCar.stop()
+myCar.checkService()
+
+console.log(myCar)
